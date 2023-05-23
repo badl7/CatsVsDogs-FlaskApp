b16e830d22d560ce1cd581b57e5b46d0931bbdaa
badl7 <btlgrbz071@gmail.com>
2023-05-24 01:52:23 +0300

    change model for slug size

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ UPLOAD_FOLDER = f'{STATIC_FOLDER}/uploads'
 MODEL_FOLDER = f'{STATIC_FOLDER}/models'
 
 
-model = tf.keras.models.load_model(f'{MODEL_FOLDER}/cats_dogs_classifierv2.h5', custom_objects={'KerasLayer': hub.KerasLayer})
+model = tf.keras.models.load_model(f'{MODEL_FOLDER}/cat_dog_classifier.h5', custom_objects={'KerasLayer': hub.KerasLayer})
 print('[INFO] : Model loaded')
 
 def predict(full_path):
     data = tf.io.read_file(full_path)
     data = tf.image.decode_jpeg(data, channels=3)
     # (224, 224, 3) > (150, 150, 3) - used a self trained model, not the inceptionV3
-    data = tf.image.resize(data, [224, 224])
+    data = tf.image.resize(data, [128, 128])
     data = np.expand_dims(data, axis=0) #Expand the shape of a data
     # rescaling
     data = data.astype('float') / 255.0
@@ -53,18 +53,16 @@ def upload_file():
 
     result = predict(full_name)
     
-    pred_label = np.argmax(result)
-    dog_prob = result[0][1]
-    cat_prob = result[0][0]
-    if pred_label==1:
-        label = 'Dog' 
-        acc = round(dog_prob * 100, 2)
+    pred_prob = result.item()
+
+    if pred_prob > .5:
+        label = 'Dog'
+        accuracy = round(pred_prob * 100, 2)
     else:
         label = 'Cat'
-        acc = round(cat_prob * 100, 2)
-    print(result)
-    
-    return render_template('predict.html', image_file_name=file.filename, label=label, acc=acc)
+        accuracy = round((1 - pred_prob) * 100, 2)
+
+    return render_template('predict.html', image_file_name=file.filename, label=label, accuracy=accuracy)
 
 
 @app.route('/upload/<filename>')
@@ -72,10 +70,7 @@ def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 def create_app():
-    
     return app
-
-
 
 if __name__ == "__main__":
     app = create_app()
